6e6cdcc85ae66a3ec3e2a0f3a6e907b3ca78ef04
kennymoody <kennymoody@gmail.com>
2026-03-10 12:47:41 +0800

    start react

---
+++ my-react-app/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    allowedHosts: ["reactserver.kennymoody.net"], // DANGER: Allows requests from any host
+    port: 5600,
+  },
+  base: "https://kennymoody.github.io/ERB-FE5-22-PP/",
+});
